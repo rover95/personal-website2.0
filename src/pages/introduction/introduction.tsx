3ee70b84fe05a37c89dbc0908de2d6efd83a0bcc
rover95 <rover95rx@gmail.com>
2021-04-10 15:43:00 +0800

    （*）

--- src/pages/introduction/introduction.tsx
+++ src/pages/introduction/introduction.tsx
@@ -97,8 +97,6 @@ function Introduction () {
               ></div>
             </div>
 
-            
-
             <div className="parallax-image-wrapper parallax-image-wrapper-50" data-anchor-target="#content-bg-3 + .gap" data-bottom-top="transform:translate3d(0px, 300%, 0px)" data-top-bottom="transform:translate3d(0px, 0%, 0px)">
               <div
                 className="parallax-image parallax-image-50"
@@ -121,7 +119,7 @@ function Introduction () {
                 }}
               >
                 <div className="head-text t-center" data-0="display:block;left:50%;top:40%;transform: translateX(-50%);transform-origin:0 0;" data-300="top:-70%;">
-                  <img className="h-img"  src={getImgSrc(12)} alt=""/>
+                  <img className="h-img" src={getImgSrc(12)} alt="" />
                   <h1>{Math.round((new Date().getTime() - 799975200000) / 3600 / 1000)} </h1>
                   <p>hours ago，I was born</p>
                 </div>
@@ -142,8 +140,28 @@ function Introduction () {
                   <p>均有涉猎</p>
                 </div>
               </div>
+
+              <div className="gap gap-100 p-6">
+                <div className="card-box">
+                  <div className="card-cell">
+                    <img className="" src={getImgSrc(11)} alt="" />
+                    <h4>克隆技术</h4>
+                    <p>将复制粘贴应用到实际生产生活中</p>
+                  </div>
+                  <div className="card-cell">
+                    <img className="" src={getImgSrc(10)} alt="" />
+                    <h4>摄影</h4>
+                    <p>用光的艺术</p>
+                  </div>
+                  <div className="card-cell">
+                    <img className="" src={getImgSrc(13)} alt="" />
+                    <h4>跋山涉水</h4>
+                    <p>终日昏昏醉梦间，忽闻春尽强登山</p>
+                  </div>
+                </div>
+              </div>
               <div className="p-8">
-                <div className="l" style={{opacity:slideBox.r_hover?0.5:1}} onMouseEnter={()=>onInfoBoxMouseEnter('l')} onMouseLeave={()=>onInfoBoxMouseLeave()}>
+                <div className="l" style={{ opacity: slideBox.r_hover ? 0.5 : 1, backgroundImage: `url(${getImgSrc(7)})` }} onMouseEnter={() => onInfoBoxMouseEnter('l')} onMouseLeave={() => onInfoBoxMouseLeave()}>
                   <div className="info">
                     <div className="title">
                       <p>lǐ xiǎnɡ </p>
@@ -155,7 +173,7 @@ function Introduction () {
                     </div>
                   </div>
                 </div>
-                <div className="r" style={{opacity:slideBox.l_hover?0.5:1}} onMouseEnter={()=>onInfoBoxMouseEnter('r')} onMouseLeave={()=>onInfoBoxMouseLeave()}>
+                <div className="r" style={{ opacity: slideBox.l_hover ? 0.5 : 1, backgroundImage: `url(${getImgSrc(8)})` }} onMouseEnter={() => onInfoBoxMouseEnter('r')} onMouseLeave={() => onInfoBoxMouseLeave()}>
                   <div className="info">
                     <div className="title">
                       <p>xiàn shí</p>
@@ -165,26 +183,6 @@ function Introduction () {
                       <p>食不饱力不足，才美不外现</p>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="gap gap-100 p-6">
-                <div className="card-box">
-                  <div className="card-cell">
-                    <img className="" src={getImgSrc(11)} alt=""/>
-                    <h4>克隆技术</h4>
-                    <p>业余时间研究克隆技术</p>
-                  </div>
-                  <div className="card-cell">
-                    <img className="" src={getImgSrc(10)} alt=""/>
-                    <h4>摄影</h4>
-                    <p>用光的艺术</p>
-                  </div>
-                  <div className="card-cell">
-                    <img className="" src={getImgSrc(13)} alt=""/>
-                    <h4>户外登山</h4>
-                    <p>在乎山水之间也</p>
-                  </div>
-                  
                 </div>
               </div>
               <div className="p-9">
@@ -211,13 +209,13 @@ function Introduction () {
                 }}
                 data-anchor-target="#content-bg-1"
               >
-                <div className="p-2" data-anchor-target="#content-bg-1" data-center-top="left:-50%;top:30%;transform: translateX(-50%);" data-top-top="left:50%;" data-bottom-bottom="left:50%;" data-center-bottom="left:250%;" >
+                <div className="p-2" data-anchor-target="#content-bg-1" data-center-top="left:-50%;top:30%;transform: translateX(-50%);" data-top-top="left:50%;" data-bottom-bottom="left:50%;" data-center-bottom="left:250%;">
                   <h3>MY TITLE</h3>
-                  <img src={getImgSrc(14)} alt=""/>
+                  <img src={getImgSrc(14)} alt="" />
                   <p>当代思考家、业余记者、无党派人士、摄影爱好者、优酷会员、饿了么超级VIP</p>
                 </div>
               </div>
-              
+
               <div className="content gap-100 p-4" id="content-bg-3">
                 <div className="text-c" data-anchor-target="#content-bg-3" data-bottom-top="top:100%;left:50%;transform: translateX(-50%)" data-bottom-bottom="top:20%;" data-center-bottom="display:block;" data-top-bottom="top:-100%;display:none;">
                   <div>
@@ -243,8 +241,11 @@ function Introduction () {
                 <div className="foot">
                   <h2>单身</h2>
                   <p>寻觅佳人</p>
-                  <p>善良，美好，<s>正年少</s></p>
+                  <p>
+                    善良，美好，<s>正年少</s>
+                  </p>
                   <p>适龄异性请想办法联系我</p>
+                  <p>爱情经不起等待，添加好友就现在</p>
                   <i className="hide">“ 英雄，愿你有一份不悔的爱情 ”</i>
                 </div>
 
